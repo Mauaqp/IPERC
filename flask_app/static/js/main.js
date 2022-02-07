7451e0f7571222f56e9e38e8358e13377ed68570
Mauaqp <peraltilla93@hotmail.com>
2022-02-07 13:54:21 -0500

    Avance. Lógica .js de probabilidad x consecuencia

---
+++ flask_app/static/js/main.js
@@ -0,0 +1,36 @@
+// Test conection js
+function myFunction() {
+    alert("main.js está conectado!");
+}
+
+// Insertar row en tbody
+let valProbabilidad = "";
+let valConsecuencia = "";
+
+// Display de probabilidad x consecuencia
+// let resultado = document.getElementById("#resultado");
+// console.log(resultado);
+
+function show(){
+    p = document.querySelector("probabilidad")
+    console.log(p)
+}
+// let probabilidad = document.getElementById("probabilidad");
+// let p_valor = probabilidad.value;
+
+// console.log(p_valor);
+
+// let consecuencia = document.getElementById("consecuencia"), 
+//     c_valor = consecuencia.value;
+
+// probabilidad.onchange=()=>calculate();
+// consecuencia.onchange=()=>calculate();
+
+// function calculate () {
+//     a = parseInt(p_valor);
+//     b = parseInt(c_valor);
+//     let cal = 0;
+//     cal = a * b;
+//     resultado.innerText = `${cal}`;
+// }
+// calculate()
